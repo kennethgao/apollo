c1f38e5524e86bc3b2fb838ab0988e99f39130ad
Rongqi Qiu <rongqi.qiu@gmail.com>
2017-10-04 17:01:16 -0700

    changed delphi_esr/mobileye/l3_perception launch scripts (#549)
    * changed delphi_esr/mobileye/l3_perception launch scripts
    
    * changed script names

--- scripts/delphi_esr_launch.sh
+++
@@ -1,1 +0,0 @@
-./bazel-bin/modules/drivers/delphi_esr/delphi_esr --flagfile=modules/drivers/delphi_esr/conf/delphi_esr.conf
--- scripts/mobileye_launch.sh
+++
@@ -1,1 +0,0 @@
-./bazel-bin/modules/drivers/mobileye/mobileye --flagfile=modules/drivers/mobileye/conf/mobileye.conf
